79ba68888de6bf0c2f5c136746aa0dfef22b5e89
Michael <michael123gs@aol.com>
2017-04-27 17:21:21 +0100

    Initial Commit

---
+++ GameCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Michael Shepherd on 27/04/2017.
+//  Copyright © 2017 Shepherd Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
