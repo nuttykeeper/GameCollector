220c4f3704e8f057814bb3d6acb4b80df3e2346f
Michael Shepherd <michael123gs@aol.com>
2017-04-28 14:57:35 +0100

    Finished App!

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -12,13 +12,25 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
 
     @IBOutlet weak var gameImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var addupdatebutton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
     
     var imagePicker = UIImagePickerController()
+    var game : Game? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imagePicker.delegate = self
+        
+        if game != nil {
+            gameImageView.image = UIImage(data: game!.image! as Data)
+            titleTextField.text = game!.title
+            
+            addupdatebutton.setTitle("Update", for: .normal)
+        } else{
+            deleteButton.isHidden = true
+        }
     }
     
     @IBAction func photosTapped(_ sender: Any) {
@@ -33,14 +45,30 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func addTapped(_ sender: Any) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        if game != nil {
+            game!.title = titleTextField.text
+            game!.image = UIImagePNGRepresentation(gameImageView.image!)! as NSData
+        }else{
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let game = Game(context: context)
+            game.title = titleTextField.text
+            game.image = UIImagePNGRepresentation(gameImageView.image!)! as NSData
+        }
         
-        let game = Game(context: context)
-        game.title = titleTextField.text
-        game.image = UIImagePNGRepresentation(gameImageView.image!)! as NSData
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+    }
+    
+    @IBAction func deleteTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(game!)
         
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
